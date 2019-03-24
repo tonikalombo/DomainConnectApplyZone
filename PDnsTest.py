af0ef80856893a24c40e18327b9b6dbed70f8133
David Nadoba <dnadoba@gmail.com>
2019-03-24 19:51:25 +0100

    work in progress

--- PDnsTest.py
+++ PDnsTest.py
@@ -1,6 +1,7 @@
 import json
 import requests
 
+
 from DomainConnect import *
 
 def ReadZoneRecords(host, serverId, domain, apiKeySecret):
@@ -15,13 +16,21 @@ def ReadZoneRecords(host, serverId, domain, apiKeySecret):
             domain_connect_format = ConvertPdnsResponseToDomainConnectFormat(zone_record)                
             #domain_connect_template["records"].append(domain_connect_format) 
             domain_connect_template.append(domain_connect_format)                                
-      return domain_connect_template   
+      return domain_connect_template
     else:
         return {'response_text' : r.text, 'status_code' : r.status_code}
 
 def WriteZoneRecords(host, serverId, domain, zone_records, apiKeySecret):
-    r = requests.put('https://api.godaddy.com/v1/domains/' + domain + '/records', headers={'Authorization' : 'sso-key ' + apiKeySecret}, json=zone_records)
-    return r.status_code==200
+    url = host + "/api/v1/servers/" + serverId + "/zones/" + domain
+    for zone_record in zone_records:
+        payload = ConvertDomainConnectFormatToPdnsFormat(zone_record)
+        print(payload)
+        r = requests.request("PATCH", url, headers={'X-API-Key': apiKeySecret}, data=json.dumps(payload))
+        if r.status_code != 204:
+            print(r.content)
+        else:
+            print("OK")
+
 def ConvertPdnsResponseToDomainConnectFormat(zone_record):
     domain_connect_template = {
             "type": zone_record.get('type'),
@@ -37,8 +46,25 @@ def ConvertPdnsResponseToDomainConnectFormat(zone_record):
           }
     return domain_connect_template
 
+def ConvertDomainConnectFormatToPdnsFormat(zone_record):
+    return {
+        "rrsets": [{
+          "name": zone_record.get('name'),
+          "type": zone_record.get('type'),
+          "ttl": zone_record.get('ttl'),
+          "changetype": "REPLACE",
+          "records": [
+            {
+              "content": zone_record.get('data'),
+              "disabled": False,
+            }
+          ]
+        }]
+    }
+
 def Test():
-    domain = "example.org"
+    domain = "david.org"
+    host = "foo"
     serverId = "localhost"    
     host = "http://172.38.249.159:8081"
     apiKey = "111"
@@ -49,18 +75,18 @@ def Test():
     print(json.dumps(zone_records, indent=2))
 
     dc = DomainConnect(providerId, serviceId)
-    p = dc.Prompt()
+    p = {'verifytxt': 'foo', 'spftxt': 'bar'} #dc.Prompt()
 
     new_r, deleted_r, final_r = dc.Apply(zone_records, domain, host, p)
 
     print("New Records")
     print(json.dumps(new_r, indent=2))
-    print("Deleted Records")
-    print(json.dumps(deleted_r, indent=2))
-    print("Final Records")
-    print(json.dumps(final_r, indent=2))
+    # print("Deleted Records")
+    # print(json.dumps(deleted_r, indent=2))
+    # print("Final Records")
+    # print(json.dumps(final_r, indent=2))
 
-    print(DeleteZoneRecords(host, serverId, domain, deleted_r, apiKey))
+    # print(DeleteZoneRecords(host, serverId, domain, deleted_r, apiKey))
     print(WriteZoneRecords(host, serverId, domain, final_r, apiKey))
 
 Test()
